7baff68eb15e1255c9733c77c1ae7816bc1e3312
Samuel Terefe <samuelbryo@gmail.com>
2016-11-15 12:43:43 -0800

    created cookie.jar

--- login_scrapper.py
+++ login_scrapper.py
@@ -1,4 +1,4 @@
-import requests, webbrowser
+import requests, webbrowser, cookielib
 from lxml import html
 
 
@@ -7,23 +7,27 @@ go("https://piazza.com")
 b = get_browser()
 b.showforms()
 
-USERNAME = "*******"
-PASSWORD = "*******"
+USERNAME = "nehe8v10"
+PASSWORD = "test123"
 
 LOGIN_URL = "https://piazza.com/"
-
+load_cookies("./cookie.jar")
 def main():
     session_requests = requests.session()
     result = session_requests.get(LOGIN_URL)
     tree = html.fromstring(result.text)
 
     payload = {
+        "action": 'login',
         "username" : USERNAME,
         "password" : PASSWORD
     }
 
-    result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
 
+    result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
+    print(result)
+    save_cookies("./cookie.jar")
+    r = session_requests.get('https://piazza.com/class/is2hozuzije420')
 webbrowser.open_new_tab(LOGIN_URL)
 if __name__ == '__main__':
     main()
