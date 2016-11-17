baacbf739d5767b9240cc66d3f3e76b7b3e78dbd
Samuel <samuelbryo@gmail.com>
2016-11-17 13:14:49 -0800

    map_it file created

---
+++ map_it.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+#map_it.py :- Launches a map in the broswer using an address from the command line or clipboard
+
+
+import webbrowser,sys
+if len(sys.argv) > 1:
+    #Get address from a command line.
+    address = ' '.join(sys.argv[1:])
+    webbrowser.open(address)
