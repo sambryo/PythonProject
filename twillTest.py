01cc60aca6d6972c7d5a2eca30861616f3a68166
Samuel <samuelbryo@gmail.com>
2016-11-17 16:57:46 -0800

    tweak twill module a little more

--- twillTest.py
+++ twillTest.py
@@ -1,13 +1,20 @@
+import twill, io
 from twill.commands import *
-go("https://piazza.com")
+
+debug("http","on")
+#SIO = io.StringIO()
+#twill.set_output(SIO)
+
 b = get_browser()
+#redirect_output("twill_out")
+go("https://piazza.com")
+#print(SIO.getvalue())
 
 b.showforms()
 
-fv("1", "email", '********')
-fv("1", "password", "******")
+fv("1", "email", 'nehe8v10@gmail.com')
+fv("1", "password", "test123")
 
-submit('0')
-
-
-
+#save_html("twill1")
+b.submit("0")
+#save_html("twill2")
--- webscraping.py
+++ webscraping.py
@@ -1,2 +1,14 @@
-import webbrowser
-webbrowser.open("https://piazza.com/account/login")
+from selenium import webdriver
+from selenium.webdriver.support import ui
+from selenium.webdriver.common.keys import Keys
+def page_is_loaded(driver):
+    return driver.find_element_by_tag_name("body") != None
+driver = webdriver.Firefox()
+driver.get("https://www.facebook.com/")
+wait = ui.WebDriverWait(driver, 10)
+wait.until(page_is_loaded)
+email_field = driver.find_element_by_id("email")
+email_field.send_keys("user@example.com")
+password_field = driver.find_element_by_id("pass")
+password_field.send_keys("password")
+password_field.send_keys(Keys.RETURN)
